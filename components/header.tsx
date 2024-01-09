caa527b22fac4750fbef69bc4c2731fadd183b40
NicoleM00 <NicoleM00@users.noreply.github.com>
2024-01-09 10:48:41 +0100

    added 'use client' to header

--- components/header.tsx
+++ components/header.tsx
@@ -1,28 +1,22 @@
+'use client'
 import Link from "next/link";
 import Image from "next/image";
 
 export default function Header() {
     return (
         <nav>
-            <ul>
-                <li>
-                    <Link href="/">
-                        <Image src="/logo.png" alt="Logo" width={100} height={100}></Image>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/staff">
-                        Staff page
-                    </Link>
-                </li>
-                {/* <li>
-                    <Link href="/admin">
-                    </Link>
-                </li> */}
-                <li>
-                    <Link href="/">Login</Link>
-                </li>
-            </ul>
+            <div className="hidden sm:ml-6 sm:block bg-blue-500">
+                <div className="flex space-x-4">
+                    <a href="/" className ="bg-blue-500 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">
+                    <Image src="/logo.png" alt="Logo" width={30} height={30}></Image>
+                    </a>
+                    <a href="/staff" className="text-white hover:bg-blue-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Staff</a>
+                    {/*
+                    <a href="/admin" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Admin</a>
+                    */}
+                    <a href="/" className="text-white hover:bg-blue-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Login</a>
+                </div>
+            </div>
         </nav>
     )
 }
