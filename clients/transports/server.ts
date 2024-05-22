d5dbda77085a541e91ab92d1ff91d73eecad2d57
JonM0 <61517667+JonM0@users.noreply.github.com>
2024-05-22 15:42:52 +0200

    Add extra transports

--- clients/transports/server.ts
+++ clients/transports/server.ts
@@ -4,5 +4,8 @@ import { GrpcTransport } from "@protobuf-ts/grpc-transport";
 const channelCredentials = ChannelCredentials.createInsecure();
 
 export const serverTransport = {
-    flightManager: new GrpcTransport({ host: process.env.SVC_FLIGHTMNGR_HOST, channelCredentials })
+    flightManager: new GrpcTransport({ host: process.env.SVC_FLIGHTMNGR_HOST, channelCredentials }),
+    ticketSvc: new GrpcTransport({ host: process.env.SVC_TICKET_HOST, channelCredentials }),
+    saleSvc: new GrpcTransport({ host: process.env.SVC_SALE_HOST, channelCredentials }),
+    validationSvc: new GrpcTransport({ host: process.env.SVC_VALIDATION_HOST, channelCredentials }),
 } as const;
--- env.d.ts
+++ env.d.ts
@@ -2,6 +2,9 @@ declare namespace NodeJS {
     interface ProcessEnv {
         NEXT_PUBLIC_GRPC_URL: string
         SVC_FLIGHTMNGR_HOST: string
+        SVC_TICKET_HOST: string
+        SVC_SALE_HOST: string
+        SVC_VALIDATION_HOST: string
     }
 }
 
