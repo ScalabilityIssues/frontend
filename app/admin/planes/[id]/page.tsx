e26e5ebae4d334196e725ddaf65d3584a47005bf
MrCosta57 <58561655+MrCosta57@users.noreply.github.com>
2024-06-06 10:50:08 +0200

    Removed plane delete

--- app/admin/planes/[id]/page.tsx
+++ app/admin/planes/[id]/page.tsx
@@ -3,12 +3,10 @@
 import { Plane } from "@/clients/gen/flightmngr/planes";
 import { PlanesClient } from "@/clients/gen/flightmngr/planes.client";
 import { webTransport } from "@/clients/transports/web";
-import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 
 export default function PlanesIdAdmin({ params }: { params: { id: string } }) {
-    const router = useRouter();
     const clientPlanes = new PlanesClient(webTransport);
     const [plane, setPlane] = useState<Plane>();
 
@@ -17,12 +15,6 @@ export default function PlanesIdAdmin({ params }: { params: { id: string } }) {
             setPlane(result.response);
         });
     }, []);
-
-    const handleDelete = () => {
-        clientPlanes.deletePlane({ id: params.id }).then(() => {
-            window.location.reload();
-        })
-    }
 
     return (
         <div className="container mx-auto p-4">
@@ -35,16 +27,6 @@ export default function PlanesIdAdmin({ params }: { params: { id: string } }) {
                         <p className="block text-black-700"><span className="font-semibold">Model:</span> {plane?.model}</p>
                         <p className="block text-black-700"><span className="font-semibold">Cabin Capacity (number of persons):</span> {plane?.cabinCapacity}</p>
                         <p className="block text-black-700"><span className="font-semibold">Cargo Capacity (in kg):</span> {plane?.cargoCapacityKg}</p>
-                        <p className={`block ${plane?.deleted ? 'text-red-600' : 'text-green-600'}`}><span className="font-semibold">Deleted:</span> {plane?.deleted ? "Yes" : "No"}</p>
-                        {!plane?.deleted && (
-                            <button
-                                type="button"
-                                onClick={handleDelete}
-                                className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600"
-                            >
-                                Delete
-                            </button>
-                        )}
                     </div>
                 </>
             )}
